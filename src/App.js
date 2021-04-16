a595bcaa56dc3268548cac4c07143ef1b4654220
ararita <munjizavesna@gmail.com>
2021-04-16 15:06:33 +0200

    Fetch data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+// import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [data, setData] = useState([]);
+  const [error, setError] = useState("");
+
+  //https://hacker-news.firebaseio.com/v0/item/12.json?print=pretty
+  //https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty
+
+  useEffect(() => {
+    fetch(`https://hacker-news.firebaseio.com/v0/item/12.json?print=pretty`)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error("Could not fetch data");
+      })
+      .then((myJson) => setData(myJson))
+      .catch((err) => {
+        console.log("error", err);
+        setError(err.message);
+      });
+  }, []);
+
+  if (data) {
+    return (
+      <div className="App">
+        <div>{JSON.stringify(data)}</div>
+        <h2>{data.by}</h2>
+      </div>
+    );
+  }
 }
 
 export default App;
