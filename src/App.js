df2f3142ffb626d94939fc3c259ecf9cff7233be
ararita <munjizavesna@gmail.com>
2021-04-21 21:05:52 +0200

    Render stories, resolve issue with two api calls

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 // import "./App.css";
 import { useState, useEffect } from "react";
 
+function getStory(id) {
+  return fetch(
+    `https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`
+  ).then((response) => {
+    if (response.ok) {
+      return response.json();
+    }
+    throw new Error("Could not fetch story id");
+  });
+}
+
 function App() {
-  const [data, setData] = useState([]);
-  const [storyId, setStoryId] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const [stories, setStories] = useState([]);
+  const [isLoaded, setIsLoaded] = useState(false);
   const [error, setError] = useState("");
 
   //https://hacker-news.firebaseio.com/v0/item/12.json?print=pretty
@@ -20,51 +30,36 @@ function App() {
       })
       .then(
         (myJson) => {
-          // console.log(myJson);
-          setLoading(true);
-          setData(myJson.splice(20));
+          const myPromises = [];
           for (let id of myJson) {
-            // console.log("id", id);
-            fetch(
-              `https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`
-            )
-              .then((response) => {
-                if (response.ok) {
-                  return response.json();
-                }
-                throw new Error("Could not fetch story id");
-              })
-              .then((myId) => {
-                console.log(myId);
-                setStoryId(myId);
-              });
+            myPromises.push(getStory(id));
           }
+          return Promise.all(myPromises).then((stories) => {
+            setStories(stories);
+            setIsLoaded(true);
+          });
         },
         (error) => {
-          setLoading(true);
           setError(error);
         }
       );
-    // .catch((err) => {
-    //   console.log("error", err);
-    //   setError(err.message);
-    // });
+    // .catch((err) => setError(err.message));
   }, []);
-
+  console.log(stories);
   if (error) {
     return <div>Error: {error.message}</div>;
-  } else if (!loading) {
+  } else if (!isLoaded) {
     return <div>Loading...</div>;
   } else {
     return (
       <div className="App">
-        {/* <div>{JSON.stringify(storyId)}</div> */}
-        <div>
-          {" "}
-          {data.map((story) => (
-            <h1 key={story.id}>{story.title}</h1>
-          ))}
-        </div>
+        {/* <pre>{JSON.stringify(stories, null, 2)}</pre>{" "} */}
+        {stories.map((story, id) => (
+          <div key={id}>
+            <h3>by: {story.by}</h3>
+            <a href={story.url}>{story.title}</a>
+          </div>
+        ))}
       </div>
     );
   }
